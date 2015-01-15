eb942b28eb543e694724817c4eb8803e1786e4cb
Andrea Bolognani <eof@kiyuko.org>
2015-01-15 11:53:22 +0100

    Move fatal error handling to a separate function.

--- main.go
+++ main.go
@@ -8,24 +8,26 @@ import (
 	"golang.org/x/net/html/atom"
 )
 
+func die(rc int, err error) {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(rc)
+}
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage:", os.Args[0], "URL")
-		os.Exit(1)
+		die(1, fmt.Errorf("Usage: %s URL", os.Args[0]))
 	}
 
 	url := os.Args[1]
 
 	res, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		die(2, err)
 	}
 
 	doc, err := html.Parse(res.Body)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		die(2, err)
 	}
 	defer res.Body.Close()
 
